2b3a372cac885a254cf4d281d99ddc3bf0a6bb5f
Jack <90474780+jto-d@users.noreply.github.com>
2024-02-15 18:06:33 -0500

    home page + headings + tags

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Tag = () => {
+const Container = styled.div`
+    padding: 10px 30px;
+    flex-shrink: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+    border-radius: 50px;
+    background-color: ${props => props.color};
+    opacity: ${props => props.opacity};
+
+    p {
+        color: #FBF9FF;
+        text-align: center;
+        font-family: "Proxima Nova";
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+`
+
+const Tag = ({ text, color, opacity }) => {
   return (
-    <div>Tag</div>
+    <Container color={color} opacity={opacity}>
+        <p>
+            {text}
+        </p>
+    </Container>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Tag } from '../components'
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #111111;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+`
+
+const Heading = styled.h1`
+  text-align: center;
+  font-family: Montserrat;
+  font-size: 110px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+
+  background: linear-gradient(92deg, #F6AC5D 19.68%, #DB6575 57.48%, #CA1E54 107.32%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const Subheading = styled.h2`
+  color: #FBF9FF;
+  text-align: center;
+  font-family: "Proxima Nova";
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const Tags = styled.div`
+  width: 70%;
+  flex-shrink: 0;
+
+
+  gap: 20px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+`
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+      <Heading>e-club design</Heading>
+      <Subheading>student-run creative design hub @ princeton e-club</Subheading>
+      <Tags>
+        <Tag color={"#F6AC5D"} opacity={0.85} text={"graphic design"} />
+        <Tag color={"#F6AC5D"} opacity={0.75} text={"ui/ux"} />
+        <Tag color={"#DB6575"} opacity={0.85} text={"branding"} />
+        <Tag color={"#CA1E54"} opacity={0.75} text={"marketing"} />
+        <Tag color={"#CA1E54"} opacity={0.85} text={"+ more!"} />
+      </Tags>
+    </Container>
+    
   )
 }
 
